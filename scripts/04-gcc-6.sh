d69533f2c047dc170d2bdda9886fb82b754871cd
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-04-09 10:00:08 +0200

    test gcc compile with BUILD_GCC_VERSION

--- scripts/04-gcc-6.sh
+++ scripts/04-gcc-6.sh
@@ -8,25 +8,22 @@ fi
 # install gcc version 6.4.0
 # see: https://wiki.gentoo.org/wiki/Upgrading_GCC
 
-# FIXME: move GCC_VERSION as BUILD_GCC_VERSION to config.sh for unified configuration
-GCC_VERSION="6.4.0"
-
 sudo mkdir -p /etc/portage/package.unmask
 cat <<'DATA' | sudo tee -a /etc/portage/package.unmask/gcc
-=sys-devel/gcc-GCC_VERSION
+=sys-devel/gcc-BUILD_GCC_VERSION
 DATA
-sudo sed -i 's/GCC_VERSION/'"$GCC_VERSION"'/g' /etc/portage/package.unmask/gcc
+sudo sed -i 's/BUILD_GCC_VERSION/'"$BUILD_GCC_VERSION"'/g' /etc/portage/package.unmask/gcc
 sudo cat /etc/portage/package.unmask/gcc
 
 # DEBUG: check if version is correctly set
 sudo cat /etc/portage/package.unmask/gcc
 
-sudo emerge --oneshot sys-devel/gcc:${GCC_VERSION}
+sudo emerge --oneshot sys-devel/gcc:${BUILD_GCC_VERSION}
 
 # DEBUG:
 sudo gcc-config --list-profiles
 
-sudo gcc-config "x86_64-pc-linux-gnu-${GCC_VERSION}"
+sudo gcc-config "x86_64-pc-linux-gnu-${BUILD_GCC_VERSION}"
 
 # DEBUG:
 sudo gcc-config --list-profiles
