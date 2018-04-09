d9597cb9a28f3d0df619992dd33e679d84096247
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-04-09 14:08:10 +0200

    disabled debugging stuff

--- scripts/04-gcc-6.sh
+++ scripts/04-gcc-6.sh
@@ -13,7 +13,7 @@ fi
 # install gcc version 6.4.0
 # see: https://wiki.gentoo.org/wiki/Upgrading_GCC
 
-echo "Will install GCC ${BUILD_GCC_VERSION} ..."
+echo "Installing GCC ${BUILD_GCC_VERSION} ..."
 
 sudo mkdir -p /etc/portage/package.unmask
 cat <<'DATA' | sudo tee -a /etc/portage/package.unmask/gcc
@@ -23,27 +23,27 @@ sudo sed -i 's/BUILD_GCC_VERSION/'"$BUILD_GCC_VERSION"'/g' /etc/portage/package.
 sudo cat /etc/portage/package.unmask/gcc
 
 # DEBUG: check if version is correctly set
-sudo cat /etc/portage/package.unmask/gcc
+#sudo cat /etc/portage/package.unmask/gcc
 
 sudo emerge --oneshot sys-devel/gcc:${BUILD_GCC_VERSION}
 
 # DEBUG:
-sudo gcc-config --list-profiles
+#sudo gcc-config --list-profiles
 
 sudo gcc-config "x86_64-pc-linux-gnu-${BUILD_GCC_VERSION}"
 
 # DEBUG:
-sudo gcc-config --list-profiles
+#sudo gcc-config --list-profiles
 
 sudo emerge --oneshot sys-devel/libtool
 
 # DEBUG:
-sudo gcc --version
+#sudo gcc --version
 
 sudo emerge --depclean sys-devel/gcc
 
 # DEBUG:
-sudo gcc-config --list-profiles
+#sudo gcc-config --list-profiles
 
 # FIXME not needed as we do a complete rebuild ...
 #sudo revdep-rebuild --library 'libstdc++.so.6' -- --exclude gcc
