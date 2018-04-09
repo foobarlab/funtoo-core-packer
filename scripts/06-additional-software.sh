28b5849ce7c1a9200361034445ade4d2ad27d1d6
Martin Eisenbarth <eyesee@foobarlab.net>
2018-04-09 17:42:41 +0200

    zerofree should be already included in stage3

--- scripts/06-additional-software.sh
+++ scripts/06-additional-software.sh
@@ -18,7 +18,7 @@ sudo emerge -v sys-power/acpid
 sudo rc-update add acpid default
 
 # this is needed for compacting free space (most likely already installed in stage3 box) 
-sudo emerge -v sys-fs/zerofree
+#sudo emerge -v sys-fs/zerofree
 
 # virtualbox advanced networking only (see: https://wiki.gentoo.org/wiki/VirtualBox#Gentoo_guests)
 sudo emerge -v sys-apps/usermode-utilities
