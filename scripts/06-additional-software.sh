eae32fca7f90440e749eda7ad26ed1e2d1ff649f
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-04-09 15:19:14 +0200

    added syslog + cron services

--- scripts/06-additional-software.sh
+++ scripts/06-additional-software.sh
@@ -5,9 +5,17 @@ if [ -z ${BUILD_RUN:-} ]; then
   exit 1
 fi
 
+# logging facility: rsyslog
+sudo emerge -v app-admin/rsyslog
+sudo rc-update add rsyslog default
+	
+# cron service: cronie
+sudo emerge -v sys-process/cronie
+sudo rc-update add cronie default
+
 # this is needed if virtualbox should gracefully shutdown on close
 sudo emerge -v sys-power/acpid
-sudo rc-update add acpid default 
+sudo rc-update add acpid default
 
 # this is needed for compacting free space (most likely already installed in stage3 box) 
 sudo emerge -v sys-fs/zerofree
