ce15068c0be21a19d940097a8910029beb265393
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-04-09 09:24:28 +0200

    removed optional software

--- scripts/06-additional-software.sh
+++ scripts/06-additional-software.sh
@@ -5,9 +5,6 @@ if [ -z ${BUILD_RUN:-} ]; then
   exit 1
 fi
 
-# FIXME: add rsyslog or some other logging facility
-# FIXME: add cronie or some other cron tool
-
 # this is needed if virtualbox should gracefully shutdown on close
 sudo emerge -v sys-power/acpid
 sudo rc-update add acpid default 
@@ -28,26 +25,3 @@ sudo emerge -v net-misc/bridge-utils
 # install guest additions from inside the vm, see: https://wiki.gentoo.org/wiki/VirtualBox
 # FIXME: this is neither working, installs some older version (5.2.4)
 #sudo emerge -v app-emulation/virtualbox-guest-additions
-
-# configure as needed ---- the stuff below is quite optional
-
-# gentoo/funtoo related helper tools
-sudo emerge -v app-portage/ufed app-portage/eix
-
-# additional helpers/tools for the commandline
-# TODO some more cmdline helpers: dig, mtr, HTTPie, aria2, ripgrep, exa, fzf, icdiff, progress, pv, Diffoscope
-sudo emerge -v app-misc/mc app-misc/screen app-misc/tmux app-misc/byobu sys-fs/ncdu \
-			   www-client/links net-ftp/ncftp app-admin/apg app-shells/bash-completion
-
-# additional tools for status monitoring
-sudo emerge -v sys-process/htop net-analyzer/iptraf-ng sys-process/glances
-
-# TODO add zsh? (also include http://ohmyz.sh ?)
-# TODO add localepurge?
-# TODO add spectre-metdown-checker in /usr/local/src? (git clone https://github.com/speed47/spectre-meltdown-checker.git)
-
-# DEBUG: show eselect options
-# FIXME: configure as needed with eselect
-sudo eselect
-	
-# FIXME: put any additional configuration here
