0bc8c15ec1fd1b42166fdc428b7349a5c4c4c37c
Martin Eisenbarth <eyesee@foobarlab.net>
2018-04-11 07:26:57 +0200

    version bump, testdrive gcc 6.4.0

--- scripts/01-prepare.sh
+++ scripts/01-prepare.sh
@@ -22,7 +22,7 @@ sudo epro mix-ins +no-systemd
 # FIXME replace /etc/motd - use a template ...
 sudo rm -f /etc/motd
 cat <<'DATA' | sudo tee -a /etc/motd
-Funtoo GNU/Linux (core) - Experimental Vagrant box v0.0.16
+Funtoo GNU/Linux (core) - Experimental Vagrant box v0.0.17
 Build by Foobarlab
 DATA
 
