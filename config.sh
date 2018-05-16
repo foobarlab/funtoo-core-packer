27879ef79dac562f60056b55a271e8261774cc10
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-05-16 07:04:28 +0200

    version bump

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export BUILD_BOX_NAME="funtoo-core"
-export BUILD_BOX_VERSION="0.1.11"
+export BUILD_BOX_VERSION="0.1.12"
 
 export BUILD_PARENT_BOX_NAME="funtoo-stage3"
 export BUILD_PARENT_BOX_VAGRANTCLOUD_NAME="foobarlab/funtoo-stage3"
-export BUILD_PARENT_BOX_VAGRANTCLOUD_VERSION="2018.05.05"
+export BUILD_PARENT_BOX_VAGRANTCLOUD_VERSION="2018.05.12"
 
 export BUILD_GUEST_TYPE="Gentoo_64"
 export BUILD_GUEST_CPUS="4"
