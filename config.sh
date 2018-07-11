7dcd08e2396b30e01cdca6dc7d1192d228eecb5c
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-07-11 12:49:32 +0200

    updated virtualbox additions to 5.2.14

--- config.sh
+++ config.sh
@@ -19,7 +19,7 @@ export BUILD_BOX_PROVIDER="virtualbox"
 
 export BUILD_TIMESTAMP="$(date --iso-8601=seconds)"
 
-export BUILD_BOX_RELEASE_NOTES="GCC 5.4.0-r1, VirtualBox Guest Additions 5.2.12"	# edit this to reflect actual setup
+export BUILD_BOX_RELEASE_NOTES="GCC 5.4.0-r1, VirtualBox Guest Additions 5.2.14"	# edit this to reflect actual setup
 
 BUILD_BOX_DESCRIPTION="$BUILD_BOX_NAME version $BUILD_BOX_VERSION"
 if [ -z ${BUILD_NUMBER+x} ] || [ -z ${BUILD_TAG+x} ]; then
