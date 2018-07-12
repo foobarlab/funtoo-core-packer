a23b5d949145c6936dcb6197d0936216bb314144
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-07-12 14:58:48 +0200

    new versioning scheme (integrating build number and retaining semantic versioning scheme)

--- version.sh
+++ version.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-# this script will read the current box version or generate a new box version (format: major.minor.buildnumber)
-
-export BUILD_VERSION=$(<version)
+# this script will use an existing box version
+# or generate a new box version in semantic
+# versioning format: major.minor.buildnumber
+# as required by vagrant
 
 if [ -f build_version ]; then
-	export BUILD_BOX_VERSION=$(<build_version)
+	BUILD_BOX_VERSION=$(<build_version)
 else
-	export BUILD_BOX_VERSION=$BUILD_VERSION.$(date -u +%Y%m%d)
-	echo $BUILD_BOX_VERSION > build_version
+	# get major version (must exist as file 'version'):
+	BUILD_MAJOR_VERSION=$(<version)
+	# generate minor version (date in format YYMMDD):
+	BUILD_MINOR_VERSION=$(date -u +%y%m%d)
+	# take existing env var BUILD_NUMBER, increment the one stored in
+	# file 'build_number' or initialize a new one beginning with 100000:
+	if [ -z ${BUILD_NUMBER:-} ] ; then
+		if [ -f build_number ]; then
+			# read from file and increase by one
+			BUILD_NUMBER=$(<build_number)
+			BUILD_NUMBER=$((BUILD_NUMBER+1))
+		else
+			# init new number with high value 
+			BUILD_NUMBER=100000
+		fi
+		# store for later reuse in file 'build_number'
+		echo $BUILD_NUMBER > build_number
+		export BUILD_NUMBER
+	fi
+	BUILD_BOX_VERSION=$BUILD_MAJOR_VERSION.$BUILD_MINOR_VERSION.$BUILD_NUMBER
 fi
+
+# store in env var
+export BUILD_BOX_VERSION
+
+# store in file 'build version'
+echo $BUILD_BOX_VERSION > build_version
