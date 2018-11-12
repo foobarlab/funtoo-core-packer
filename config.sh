451a063c67c5f1974cc3917ec76041818cf87a31
Martin Eisenbarth <eyesee@foobarlab.net>
2018-11-12 08:51:33 +0100

    virtualbox info update

--- config.sh
+++ config.sh
@@ -19,7 +19,7 @@ export BUILD_BOX_PROVIDER="virtualbox"
 
 export BUILD_TIMESTAMP="$(date --iso-8601=seconds)"
 
-export BUILD_BOX_RELEASE_NOTES="Funtoo 1.2, Debian Kernel, GCC 7.3.1, VirtualBox Guest Additions 5.2.20"	# edit this to reflect actual setup
+export BUILD_BOX_RELEASE_NOTES="Funtoo 1.2, Debian Kernel, GCC 7.3.1, VirtualBox Guest Additions 5.2.22"	# edit this to reflect actual setup
 
 BUILD_BOX_DESCRIPTION="$BUILD_BOX_NAME version $BUILD_BOX_VERSION"
 if [ -z ${BUILD_TAG+x} ]; then
