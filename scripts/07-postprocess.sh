806d506cd226088a357c8ab3a1fa137a5ba4ef73
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-11-26 11:23:10 +0100

    add postprocess step: fix and configure some packages

---
+++ scripts/07-postprocess.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -uex
+
+if [ -z ${BUILD_RUN:-} ]; then
+  echo "This script can not be run directly! Aborting."
+  exit 1
+fi
+
+# net-mail/mailbase: adjust permissions as recommended during install 
+sudo chown root:mail /var/spool/mail/
+sudo chmod 03775 /var/spool/mail/
+
+# sys-apps/mlocate: add shared folder (usually '/vagrant') to /etc/updatedb.conf prune paths to avoid leaking shared files
+sudo sed -i 's/PRUNEPATHS="/PRUNEPATHS="\/vagrant /g' /etc/updatedb.conf
+
+# check dynamic linking consistency
+sudo revdep-rebuild
+ 
