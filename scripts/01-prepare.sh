8c6f9d2648c57fa26d2ca770f8a6ff1b965cf2f7
Martin Eisenbarth <martin.eisenbarth@terralumi.com>
2018-12-19 15:36:35 +0100

    disable some USE flags for grub

--- scripts/01-prepare.sh
+++ scripts/01-prepare.sh
@@ -33,6 +33,7 @@ sys-firmware/intel-microcode initramfs
 DATA
 
 cat <<'DATA' | sudo tee -a /etc/portage/package.use/vbox-defaults
+sys-boot/grub -fonts -themes -truetype 
 app-misc/mc -edit -slang
 DATA
 
